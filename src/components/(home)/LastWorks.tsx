3a56e61f60d514b0f4ca5f23fd739f29ebe8bf8e
Patrick Bartosik <patrickbartosik@MBP-de-Patrick.lan>
2024-04-20 13:45:34 +0200

    :lipstick: finish LastedWorks with css animation

--- src/components/(home)/LastWorks.tsx
+++ src/components/(home)/LastWorks.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Link from 'next/link';
-import { ArrowRight } from 'lucide-react';
+import { ArrowRight, ArrowUpRight } from 'lucide-react';
+import Image from 'next/image';
 
 const LastWorksItems = () => [
-    { id: 1, title: 'E-commerce Website', image: '/img/lastWorks/ecommerce.jpg' },
-    { id: 2, title: 'Portfolio Website', image: '/img/lastWorks/portfolio.jpg' },
-    { id: 3, title: 'Blog Website', image: '/img/lastWorks/blog.jpg' },
-    { id: 4, title: 'Web Application', image: '/img/lastWorks/webApp.jpg' },
-    { id: 5, title: 'Landing Page', image: '/img/lastWorks/landingPage.jpg' },
+    { id: 1, title: 'E-commerce Website', image: '/img/lastWorks/project1.png' },
+    { id: 2, title: 'Portfolio Website', image: '/img/lastWorks/project2.png' },
+    { id: 3, title: 'Blog Website', image: '/img/lastWorks/project3.png' },
+    { id: 4, title: 'Web Application', image: '/img/lastWorks/project4.png' },
+    { id: 5, title: 'Landing Page', image: '/img/lastWorks/project5.png' },
 ];
 
 function LastWorks() {
+    const [hoverIndex, setHoverIndex] = useState(-1);
+
     return (
         <MaxWidthWrapper>
             <section id='lastWorks' className='portfolio lastWorks mt-40 flex flex-col items-start justify-between w-full h-full'>
@@ -22,20 +26,30 @@ function LastWorks() {
                     </div>
                     <div className="portfolio__description flex flex-col gap-y-10">
                         <p className='portfolio__description-text font-mono'>To see all project done come check my project gallery</p>
-                        <Link href="/portfolio/all" className='portfolio__link-button'>
+                        <Link href="/portfolio/all" className='portfolio__link-button CustomButton'>
                             <span>View all Projects</span>
                         </Link>
                     </div>
                 </div>
-                <div className='portfolio__body pt-8 w-full'>
+                <div className='portfolio__body w-full pt-6 '>
                     {LastWorksItems().map((item, index) => (
-                        <div key={index} className='portfolio__item flex justify-between w-full'>
+                        <div
+                            key={item.id}
+                            className='portfolio__item flex justify-between w-full hover:cursor-pointer pt-8 relative '
+                            onMouseEnter={() => setHoverIndex(index)}
+                            onMouseLeave={() => setHoverIndex(-1)}
+                        >
                             <div className='portfolio__item-text flex gap-x-5'>
-                                <p className='portfolio__item-number'>0{item.id}</p>
+                                <p className='portfolio__item-number'>0{item.id}.</p>
                                 <h2 className='portfolio__item-title'>{item.title}</h2>
                             </div>
+                            <div className={`portfolio__item-image absolute flex justify-center items-center transition-all duration-300 ease-in-out transform ${hoverIndex === index ? 'opacity-100 scale-100' : 'opacity-0 scale-95'}`}>
+                                <Image src={item.image} alt={item.title} width={250} height={200} objectFit='cover' />
+                            </div>
                             <Link href={`/portfolio/${item.id}`}>
-                                <ArrowRight className='portfolio__item-icon cursor-pointer' />
+                                <div className={`portfolio__item-icon transition-all duration-300 ease-in-out transform ${hoverIndex === index ? '-rotate-45 text-[#E3B27D]' : 'text-white'}`}>
+                                    <ArrowRight className='cursor-pointer' size={30} />
+                                </div>
                             </Link>
                         </div>
                     ))}
