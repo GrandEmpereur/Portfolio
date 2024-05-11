9ba9c9d037a8364dbc1da53b99a15e3419d78d8e
Patrick Bartosik <patrickbartosik@MBP-de-Patrick.lan>
2024-05-11 12:54:42 +0200

    :sparkles: Services section done

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,6 +1,14 @@
 import MaxWidthWrapper from '@/components/MaxWidthWrapper'
 import React from 'react'
 import Image from 'next/image';
+import { services } from '@/lib/data/services';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
 
 function page() {
   return (
@@ -9,6 +17,38 @@ function page() {
         <div className="services__title">
           <h3 className='services__title-sub font-mono'>Services</h3>
           <h4 className='services__title-main keep-color '>This section regroup all my services and explanations</h4>
+        </div>
+
+        <div className='w-full'>
+          <Image src={'/img/services/services.png'} alt={'Image'} width={1200} height={537} style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+        </div>
+
+        <div className='flex flex-col gap-y-8'>
+          {services.map((service, index) => (
+            <div id={service.id} key={index} className="services__item rounded overflow-hidden shadow-lg relative">
+              <div className="group w-full flex justify-between">
+                <div className="services__content flex flex-col gap-y-8">
+                  <h4 className="services__title underline">{service.title}</h4>
+                  <p className="services__description w-[613px]">{service.description}</p>
+                  <div className="services__accordion">
+                  </div>
+                </div>
+
+                <div className='w-[600px]'>
+                  <Accordion type="single" collapsible>
+                    {service.accordion.map((accordion, idx) => (
+                        <AccordionItem key={idx} value={`${idx}`}>
+                          <AccordionTrigger>{accordion.title}</AccordionTrigger>
+                          <AccordionContent style={{borderBottom: '1px solid white'}}>
+                            {accordion.description}
+                          </AccordionContent>
+                        </AccordionItem>
+                    ))}
+                  </Accordion>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </section>
     </MaxWidthWrapper>
---
+++ src/lib/data/services.ts
@@ -0,0 +1,46 @@
+export const services = [
+    {
+        title: 'Web Development',
+        description: 'I develop websites using modern technologies and frameworks to create responsive and user-friendly websites.',
+        icon: '/img/services/web-development.svg',
+        accordion: [
+            {
+                title: 'Frontend Development',
+                description: 'I develop the client-side of websites using modern technologies and frameworks to create responsive and user-friendly websites.'
+            },
+            {
+                title: 'Backend Development',
+                description: 'I develop the server-side of websites using modern technologies and frameworks to create responsive and user-friendly websites.'
+            },
+            {
+                title: 'Fullstack Development',
+                description: 'I develop the client-side and server-side of websites using modern technologies and frameworks to create responsive and user-friendly websites.'
+            },
+        ],
+        buttonLabel: 'Learn More',
+        id: 'web-development',
+        link: '/services/#web-development',
+    },
+    {
+        title: 'Web Development',
+        description: 'I develop websites using modern technologies and frameworks to create responsive and user-friendly websites.',
+        icon: '/img/services/web-development.svg',
+        accordion: [
+            {
+                title: 'Frontend Development',
+                description: 'I develop the client-side of websites using modern technologies and frameworks to create responsive and user-friendly websites.'
+            },
+            {
+                title: 'Backend Development',
+                description: 'I develop the server-side of websites using modern technologies and frameworks to create responsive and user-friendly websites.'
+            },
+            {
+                title: 'Fullstack Development',
+                description: 'I develop the client-side and server-side of websites using modern technologies and frameworks to create responsive and user-friendly websites.'
+            },
+        ],
+        buttonLabel: 'Learn More',
+        id: 'web-zone',
+        link: '/services/#web-zone',
+    }
+]
