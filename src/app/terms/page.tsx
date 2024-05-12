f2dc0a3abacb5430c45bac53b42ab5ac619a3bcd
Patrick Bartosik <patrickbartosik@MBP-de-Patrick.lan>
2024-05-12 14:21:07 +0200

    :memo: ajout des mention lesga et autre au site avec leur rédaction et la mise en forme

--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -1,30 +1,6 @@
 import MaxWidthWrapper from '@/components/MaxWidthWrapper'
 import React from 'react'
 
-export function formatLegalText(rawText: string) {
-    const sections = rawText.split('\n').filter(line => line.trim() !== '');
-
-    return sections.map((section, index) => {
-        if (index === 0) { // Titre principal
-            return <h1 key={index} className="text-3xl font-bold my-4 pb-8">{section}</h1>;
-        }
-        if (/^Article \d+/.test(section)) { // Titres d'articles
-            return <h2 key={index} className="text-2xl font-semibold my-4">{section}</h2>;
-        }
-        if (/^[A-D]\./.test(section)) { // Sous-titres principaux
-            return <h3 key={index} className="text-xl font-semibold mt-4 mb-2 ml-4">{section}</h3>;
-        }
-        if (/^[a-d]\./.test(section)) { // Sous-sous-titres
-            return <h4 key={index} className="ml-8 font-semibold mt-4 mb-2 italic ">{section}</h4>;
-        }
-        if (section.startsWith('-')) { // Éléments de liste informels
-            return <p key={index} className="mb-4 ml-12 ">- {section.slice(1).trim()}</p>;
-        }
-        return <p key={index} className="mb-4 ml-4">{section}</p>; // Paragraphe normal avec indentation légère
-    });
-}
-
-
 const rawLegalText = `
 Legal Notices
 
@@ -70,7 +46,7 @@ Article 4 - Contact
 For any reporting of illegal content or activities, the User may contact the publisher at the following address: BartosikPatrickPro@gmail.com, or by registered mail with acknowledgment of receipt sent to the publisher at the coordinates specified in these legal notices.
 `;
 
-function page() {
+export default function page() {
     const formattedText = formatLegalText(rawLegalText);
     return (
         <MaxWidthWrapper>
@@ -81,4 +57,27 @@ function page() {
     )
 }
 
-export default page
+export function formatLegalText(rawText: string) {
+    const sections = rawText.split('\n').filter(line => line.trim() !== '');
+
+    return sections.map((section, index) => {
+        if (index === 0) { // Titre principal
+            return <h1 key={index} className="text-3xl font-bold my-4 pb-8">{section}</h1>;
+        }
+        if (/^Article \d+/.test(section)) { // Titres d'articles
+            return <h2 key={index} className="text-2xl font-semibold my-4">{section}</h2>;
+        }
+        if (/^[A-D]\./.test(section)) { // Sous-titres principaux
+            return <h3 key={index} className="text-xl font-semibold mt-4 mb-2 ml-4">{section}</h3>;
+        }
+        if (/^[a-d]\./.test(section)) { // Sous-sous-titres
+            return <h4 key={index} className="ml-8 font-semibold mt-4 mb-2 italic ">{section}</h4>;
+        }
+        if (section.startsWith('-')) { // Éléments de liste informels
+            return <p key={index} className="mb-4 ml-12 ">- {section.slice(1).trim()}</p>;
+        }
+        return <p key={index} className="mb-4 ml-4">{section}</p>; // Paragraphe normal avec indentation légère
+    });
+}
+
+
