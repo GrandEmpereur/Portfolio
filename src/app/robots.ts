129d06e95dfdc272f8b32f7ba09b98818f4af8cc
Patrick Bartosik <patrickbartosik@MacBook-Pro-de-Patrick.local>
2024-05-20 11:46:57 +0200

    ✨ fonctionnalité(robots.ts) : ajouter des règles pour les robots de tous les utilisateurs et Bingbot ♻️ refactoriser(robots.ts) : séparer les règles pour Applebot et Bingbot 📝 documentation(robots.ts) : ajouter des commentaires pour clarifier les règles et les paramètres du sitemap et de l'hôte

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,18 +1,28 @@
-import type { MetadataRoute } from 'next'
+import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
     return {
         rules: [
             {
-                userAgent: 'Googlebot',
-                allow: ['/'],
-                disallow: '/private/',
+                userAgent: '*',
+                allow: '/',
+                disallow: ['/private/'], // Disallow private directory for all user agents
             },
             {
-                userAgent: ['Applebot', 'Bingbot'],
-                disallow: ['/'],
+                userAgent: 'Googlebot',
+                allow: '/',
+                disallow: '/private/', // Disallow private directory for Googlebot as well
+            },
+            {
+                userAgent: 'Bingbot',
+                disallow: '/',
+            },
+            {
+                userAgent: 'Applebot',
+                disallow: '/',
             },
         ],
-        sitemap: 'https://patrick.bartosik.fr/sitemap.xml',
-    }
+        sitemap: 'https://patrick.bartosik.fr/sitemap.xml', // Ensure this URL is correct and accessible
+        host: 'https://patrick.bartosik.fr', // Optional: Specify your preferred domain
+    };
 }
