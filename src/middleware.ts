69b7fcf4a0869d0e50e6e58c77240e376347f0ec
Patrick Bartosik <patrickbartosik@MBP-de-Patrick.lan>
2024-05-21 09:29:04 +0200

    🔧 middleware.ts : supprimer le code commenté inutile pour ignorer certains fichiers dans le middleware.

--- src/middleware.ts
+++ src/middleware.ts
@@ -27,17 +27,6 @@ function getLocale(request: NextRequest): string | undefined {
 export function middleware(request: NextRequest) {
     const pathname = request.nextUrl.pathname;
 
-    // // `/_next/` and `/api/` are ignored by the watcher, but we need to ignore files in `public` manually.
-    // // If you have one
-    // if (
-    //   [
-    //     '/manifest.json',
-    //     '/favicon.ico',
-    //     // Your other files in `public`
-    //   ].includes(pathname)
-    // )
-    //   return
-
     // Check if there is any supported locale in the pathname
     const pathnameIsMissingLocale = i18n.locales.every(
         (locale) =>
