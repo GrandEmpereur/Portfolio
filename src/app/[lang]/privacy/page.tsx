23ea5a5e91c5f411d8e8f1df5d5d9d6008569cae
Patrick Bartosik <patrickbartosik@MBP-de-Patrick.lan>
2024-05-21 11:33:01 +0200

    📝 document(app/[lang]/contact/page.tsx) : ajouter une fonction asynchrone pour générer les métadonnées en fonction de la langue 📝 document(app/[lang]/gtu/page.tsx) : ajouter une fonction asynchrone pour générer les métadonnées en fonction de la langue
    📝 docs(privacy/page.tsx): update generateMetadata function to include metadata for
    privacy policy page
    🔧 chore(privacy/page.tsx): refactor page function to be default export and
    async
    
    📝 mettre à jour(page.tsx) : ajouter la fonctionnalité de formatage du texte légal avant l'affichage
    
    📝 docs(terms/page.tsx): Add metadata generation function for legal notices page
    📝 docs(terms/page.tsx): Update page component to use dynamic legal text from dictionary
    
    📝 update(en.json) : update English translations for consistency and clarity in the website content
    🌐 internationalization(en.json) : improve the English translations for better user understanding and engagement
    
    📝 translations(en.json): Update English translations for Quick Links and Legal sections
    🌐 translations(en.json): Add General Terms of Use and Privacy Policy templates in English
    
    📝 mettre à jour(legalText): ajouter des informations sur l'utilisation des "cookies" et la politique de confidentialité pour se conformer à la loi en vigueur.
    
    📝 translations(fr.json): mettre à jour les traductions françaises pour améliorer la clarté et l'expressivité des textes sur le site web.
    
    📝 translations(fr.json) : mettre à jour les traductions françaises pour refléter les modifications apportées au contenu des sections "content" et "TemplateAbout" du site web.
    
    📝 mettre à jour(fr.json) : ajouter des traductions pour les nouvelles pages TemplateGTU et TemplatePrivacyPolicy
    
    📝 ajouter(mentions-legales.json) : mettre à jour les mentions légales du site avec de nouvelles informations sur les droits des utilisateurs en matière de collecte et de traitement des données personnelles.

--- src/app/[lang]/privacy/page.tsx
+++ src/app/[lang]/privacy/page.tsx
@@ -1,216 +1,56 @@
-import MaxWidthWrapper from '@/components/MaxWidthWrapper'
-import React from 'react'
-import { formatLegalText } from '@/lib/formatLegalText'
+import MaxWidthWrapper from '@/components/MaxWidthWrapper';
+import { formatLegalText } from '@/lib/formatLegalText';
 import { Metadata } from 'next';
 import { Locale } from '@/i18nConfig';
-
-export const metadata: Metadata = {
-    title: "Bartosik Patrick - Privacy Policy ",
-    description: "Discover the cutting-edge portfolio of Bartosik Patrick, a seasoned Full Stack Developer specializing in React, Next.js, and Tailwind CSS. Dive into a showcase of innovative web applications that blend aesthetics with functionality, crafted to push the boundaries of digital experiences.",
-};
-
-const rawLegalText = `
-Privacy Policy
-
-Article 1: Preamble
-This privacy policy applies to the site: Patrick Bartosik.
-
-This privacy policy aims to expose to the users of the site:
-
-- The manner in which their personal data are collected and processed. Should be considered as personal data all data that can identify a user. This includes first name and last name, age, postal address, email address, user's location or even his IP address;
-- What rights users have regarding these data;
-- Who is responsible for the processing of the collected and processed personal data;
-- To whom these data are transmitted;
-- Optionally, the site’s policy on "cookies".
-
-This privacy policy supplements the legal notices.
-
-Article 2: General principles regarding data collection and processing
-In accordance with the provisions of Article 5 of the European Regulation 2016/679, the collection and processing of user data from the site respect the following principles:
-
-- Legality, fairness, and transparency: data can only be collected and processed with the consent of the data owner. Whenever personal data will be collected, the user will be informed that his data is being collected, and why his data is being collected;
-- Limited purposes: data collection and processing are executed to meet one or more objectives specified in these terms of use;
-- Minimizing the collection and processing of data: only the data necessary for the proper execution of the objectives pursued by the site are collected;
-- Reduced data retention in time: data is stored for a limited period of which the user is informed. If this information cannot be communicated, the user is informed of the criteria used to determine the duration of conservation;
-- Integrity and confidentiality of the collected and processed data: the data controller commits to ensure the integrity and confidentiality of the collected data.
-
-To be lawful, and in accordance with the requirements of Article 6 of the European Regulation 2016/679, the collection and processing of personal data may only occur if they comply with at least one of the following conditions:
-
-- The user has expressly consented to the processing;
-- Processing is necessary for the proper performance of a contract;
-- The processing meets a legal obligation;
-- The processing is explained by a need linked to the protection of the vital interests of the concerned individual or another person;
-- The processing can be explained by a necessity linked to the performance of a mission of public interest or that relates to the exercise of public authority;
-- The processing and collection of personal data are necessary for the legitimate private interests pursued by the data controller or by a third party.
-
-Article 3: Personal data collected and processed in the framework of site navigation
-A. Data collected and processed and collection mode
-The personal data collected on the Patrick Bartosik site are as follows:
-
-- IP addresses and browser cookies.
-- First name, last name, email address, telephone, and company.
-
-These data are collected when the user performs one of the following operations on the site:
-
-- When the user visits one of the site's pages.
-- When the user uses one of the contact forms.
-
-The data controller will keep in the computer systems of the site and under reasonable security conditions all the data collected for a duration of: 26 months.
-
-The collection and processing of the data serve the following purposes:
-
-- Analysis of site traffic.
-- To allow recontacting the user.
-
-B. Transmission of data to third parties
-The data may be transmitted to the following third party(ies):
-
-Google and Vercel
-
-C. Data hosting
-The Patrick Bartosik site is hosted by: Vercel, whose headquarters is located at the following address:
-
-San Francisco in the United States.
-
-The data collected and processed by the site are transferred to the following country(ies): France, Ireland, and the United States. This transfer of personal data outside the European Union justifies the following reasons:
-
-Location of the headquarters of third parties.
-
-Article 4: Data Controller
-A. The data controller
-The data controller for personal data is: Patrick Bartosik. He can be contacted as follows:
-
-By email: BartosikPatrickPro@gmail.com
-
-The data controller is responsible for determining the purposes and means used in the processing of personal data.
-
-B. Obligations of the data controller
-The data controller commits to protect the collected personal data, not to transmit them to third parties without the user having been informed, and to respect the purposes for which these data were collected.
-
-The site has an SSL certificate to ensure that the information and the transfer of data passing through the site are secured.
-
-An SSL ("Secure Socket Layer" Certificate) aims to secure the data exchanged between the user and the site.
-
-Moreover, the data controller commits to notify the user in case of correction or deletion of the data, unless this entails for him disproportionate formalities, costs, and steps.
-
-In the case where the integrity, confidentiality, or security of the user's personal data is compromised, the data controller commits to inform the user by any means.
-
-Article 5
-
-: User rights
-In accordance with the regulation concerning the processing of personal data, the user has the rights listed below.
-
-In order for the data controller to comply with his request, the user is required to communicate: his first name and last name as well as his email address.
-
-The data controller is required to respond to the user within a maximum of 30 (thirty) days.
-
-A. Presentation of the user's rights in data collection and processing
-a. Right of access, rectification, and right to erasure
-The user can take knowledge, update, modify or request the deletion of data concerning him, following the procedure set out below:
-
-The user must send an email to the personal data processing manager, specifying the subject of his request and using the contact email address provided above.
-
-b. Right to data portability
-The user has the right to request the portability of his personal data, held by the site, to another site, by complying with the following procedure:
-
-The user must make a request for the portability of his personal data to the data controller, by sending an email to the address provided above.
-
-c. Right to limitation and opposition of data processing
-The user has the right to request the limitation or to oppose the processing of his data by the site, without the site being able to refuse, except to demonstrate legitimate and compelling reasons, which can prevail over the interests and rights and freedoms of the user.
-
-To request the limitation of data processing or to formulate an opposition to data processing, the user must follow the following procedure:
-
-The user must make a request for limitation to the processing of his personal data to the data controller, by sending an email to the address provided above.
-
-d. Right not to be the subject of a decision based exclusively on an automated process
-In accordance with the provisions of Regulation 2016/679, the user has the right not to be the subject of a decision based exclusively on an automated process if the decision produces legal effects concerning him, or similarly significantly affects him.
-
-e. Right to decide the fate of data after death
-It is reminded to the user that he can organize what should be the future of his data collected and processed if he dies, in accordance with Law No. 2016-1321 of October 7, 2016.
-
-f. Right to seize the competent control authority
-In the event that the data controller decides not to respond to the user's request, and the user wishes to contest this decision, or, if he thinks that one of the rights listed above is infringed, he is entitled to seize the CNIL (National Commission on Informatics and Liberty, https://www.cnil.fr) or any competent judge.
-
-B. Personal data of minor persons
-In accordance with the provisions of Article 8 of the European Regulation 2016/679 and the Data Protection Act, only minors aged 15 years or more can consent to the processing of their personal data.
-
-If the user is a minor under 15 years old, the agreement of a legal representative will be required so that personal data can be collected and processed.
-
-The site publisher reserves the right to verify by any means that the user is over 15 years old, or that he will have obtained the agreement of a legal representative before browsing the site.
-
-Article 6: Use of "Cookies" files
-The site may have recourse to the techniques of "cookies".
-
-A "cookie" is a small file (less than 4 ko), stored by the site on the user's hard drive, containing information related to the user's browsing habits.
-
-These files allow him to process statistics and traffic information, facilitate navigation and improve the service for the user's comfort.
-
-For the use of "cookie" files involving the storage and analysis of personal data, the user's consent is necessarily asked.
-
-This user's consent is considered valid for a duration of 13 (thirteen) months maximum. At the end of this period, the site will again ask for the user's authorization to save "cookies" files on his hard drive.
-
-a. User's opposition to the use of "cookies" files by the site
-It is brought to the user's knowledge that he can oppose the recording of these "cookies" by configuring his navigation software.
-
-For information, the user can find at the following addresses the steps to follow in order to configure his navigation software to oppose the recording of "cookie" files:
-
-- Chrome: https://support.google.com/accounts/answer/61416?hl=en
-
-- Firefox: https://support.mozilla.org/en-US/kb/enable-and-disable-cookies-website-preferences
-
-- Safari: http://www.apple.com/legal/privacy/en-ww/
-
-- Internet Explorer: https://support.microsoft.com/en-us/help/17442/windows-internet-explorer-delete-manage-cookies
-
-- Opera: http://www.opera.com/help/tutorials/security/cookies/
-
-In the case where the user decides to disable the "cookies" files, he may continue his navigation on the site. However, any dysfunction of
-
- the site caused by this manipulation could not be considered as being due to the site publisher.
-
-b. Description of the "cookies" files used by the site
-The site publisher draws the user's attention to the fact that the following cookies are used during his navigation:
-
-Cookies authorized and recorded by browsers.
-
-By browsing on the site, it is brought to the user's knowledge that "cookie" files of third parties may be recorded.
-
-It is more particularly the following third parties:
-
-Third parties listed in Article 3.B above.
-
-Moreover, the site incorporates social network buttons, allowing the user to share his activity on the site. "Cookie" files of these social networks are therefore likely to be stored on the user's computer when he uses these features.
-
-The user's attention is drawn to the fact that these sites have their own privacy policies and terms and conditions of use possibly different from the site. The site publisher invites users to consult the privacy policies and the terms and conditions of use of these sites.
-
-Article 7: Condition of modification of the privacy policy
-This privacy policy can be consulted at any time at the address indicated below:
-
-https://Patrick.Bartosik.com/policy
-
-The site publisher reserves the right to modify it to ensure its compliance with the law in force.
-
-Therefore, the user is invited to come regularly consult this privacy policy to stay informed of the latest changes that will be brought to it.
-
-It is brought to the user's knowledge that the last update of this privacy policy occurred on: 01/01/2020.
-
-Article 8: Acceptance by the user of the privacy policy
-By browsing on the site, the user attests having read and understood this privacy policy and accepts its conditions, particularly concerning the collection and processing of his personal data, as well as the use of "cookie" files.
-`;
-
-function page({
+import React from 'react';
+import { getDictionary } from '@/get-dictionary';
+
+export async function generateMetadata({ params: { lang } }: { params: { lang: Locale } }) {
+    const dict = await getDictionary(lang);
+
+    return {
+        title: dict.TemplatePrivacyPolicy.metadata.title,
+        description: dict.TemplatePrivacyPolicy.metadata.description,
+        openGraph: {
+            title: dict.TemplatePrivacyPolicy.metadata.title,
+            description: dict.TemplatePrivacyPolicy.metadata.description,
+            url: `https://patrick.bartosik.fr/${lang}/privacy-policy`,
+            type: 'website',
+            images: [
+                {
+                    url: 'https://patrick.bartosik.fr/img/privacy-policy/privacyHero.png',
+                    width: 800,
+                    height: 600,
+                    alt: 'Privacy Policy Image of Patrick Bartosik',
+                },
+            ],
+        },
+        twitter: {
+            card: 'summary_large_image',
+            title: dict.TemplatePrivacyPolicy.metadata.title,
+            description: dict.TemplatePrivacyPolicy.metadata.description,
+            images: [
+                {
+                    url: 'https://patrick.bartosik.fr/img/privacy-policy/privacyHero.png',
+                    alt: 'Privacy Policy Image of Patrick Bartosik',
+                },
+            ],
+        },
+    };
+}
+
+export default async function page({
     params: { lang },
 }: {
     params: { lang: Locale };
 }) {
-    const formattedText = formatLegalText(rawLegalText);
+    const dict = await getDictionary(lang);
+    const formattedText = formatLegalText(dict.TemplatePrivacyPolicy.legalText.join('\n'));
     return (
         <MaxWidthWrapper>
             <div className="py-10 px-6 sm:px-10 lg:px-16">
                 {formattedText}
             </div>
         </MaxWidthWrapper>
-    )
+    );
 }
-
-export default page
--- src/app/[lang]/terms/page.tsx
+++ src/app/[lang]/terms/page.tsx
@@ -1,72 +1,56 @@
-import MaxWidthWrapper from '@/components/MaxWidthWrapper'
-import React from 'react'
-import { formatLegalText } from '@/lib/formatLegalText'
+import MaxWidthWrapper from '@/components/MaxWidthWrapper';
+import { formatLegalText } from '@/lib/formatLegalText';
 import { Metadata } from 'next';
 import { Locale } from '@/i18nConfig';
+import React from 'react';
+import { getDictionary } from '@/get-dictionary';
 
-export const metadata: Metadata = {
-    title: "Bartosik Patrick - Legal Notices ",
-    description: "Discover the cutting-edge portfolio of Bartosik Patrick, a seasoned Full Stack Developer specializing in React, Next.js, and Tailwind CSS. Dive into a showcase of innovative web applications that blend aesthetics with functionality, crafted to push the boundaries of digital experiences.",
-};
+export async function generateMetadata({ params: { lang } }: { params: { lang: Locale } }) {
+    const dict = await getDictionary(lang);
 
-const rawLegalText = `
-Legal Notices
+    return {
+        title: dict.TemplateLegalNotices.metadata.title,
+        description: dict.TemplateLegalNotices.metadata.description,
+        openGraph: {
+            title: dict.TemplateLegalNotices.metadata.title,
+            description: dict.TemplateLegalNotices.metadata.description,
+            url: `https://patrick.bartosik.fr/${lang}/legal-notices`,
+            type: 'website',
+            images: [
+                {
+                    url: 'https://patrick.bartosik.fr/img/legal-notices/legalNoticesHero.png',
+                    width: 800,
+                    height: 600,
+                    alt: 'Legal Notices Image of Patrick Bartosik',
+                },
+            ],
+        },
+        twitter: {
+            card: 'summary_large_image',
+            title: dict.TemplateLegalNotices.metadata.title,
+            description: dict.TemplateLegalNotices.metadata.description,
+            images: [
+                {
+                    url: 'https://patrick.bartosik.fr/img/legal-notices/legalNoticesHero.png',
+                    alt: 'Legal Notices Image of Patrick Bartosik',
+                },
+            ],
+        },
+    };
+}
 
-In accordance with the provisions of Articles 6-III and 19 of the Law No. 2004-575 of June 21, 2004, for Confidence in the Digital Economy, known as L.C.E.N., users and visitors of Patrick Bartosik's website are hereby informed of these legal notices.
-
-The Patrick Bartosik website is accessible at the following address: Patrick.Bartosik.fr (hereinafter "the Site"). Access and use of the Site are subject to these "Legal Notices" detailed below as well as applicable laws and/or regulations.
-
-Connecting to, using, and accessing this Site implies full and unconditional acceptance by the user of all the provisions of these Legal Notices.
-
-Article 1: Legal Information
-Under Article 6 of Law No. 2004-575 of June 21, 2004, for confidence in the digital economy, this article specifies the identity of the various parties involved in its implementation and monitoring.
-
-A. Site Publisher
-The Patrick Bartosik site is published by:
-Patrick Bartosik, having its registered office at the following address: 28bis rue Honoré Sohier, and registered under the number: 90093821800014 - Paris.
-Share capital: 1000 euros
-Phone: +33 (0)6 58 29 23 30
-Email address: BartosikPatrickPro@gmail.com
-hereinafter "the Publisher"
-
-B. Publishing Director
-The Publishing Director is:
-Bartosik Patrick
-hereinafter "the Publishing Director"
-
-C. Site Host
-The Patrick Bartosik site is hosted by:
-Vercel Inc., located at the following address: 	San Francisco in the United States.
-hereinafter "the Host"
-
-D. Users
-All Internet users who browse, read, view, and use the Patrick Bartosik site are considered users.
-hereinafter "the Users"
-
-Article 2 - Accessibility
-The Site is in principle accessible to Users 24/24h and 7/7d, except for interruption, scheduled or not, for maintenance needs or in case of force majeure.
-Should access to the Site become impossible, efforts will be made to restore access as soon as possible. The Site cannot be held responsible for any damage of any nature resulting from its unavailability.
-
-Article 3 - Applicable Law and Jurisdiction
-These Legal Notices are governed by French law. In case of dispute and in the absence of an amicable agreement, the dispute will be brought before the French courts in accordance with the rules of jurisdiction in force.
-
-Article 4 - Contact
-For any reporting of illegal content or activities, the User may contact the publisher at the following address: BartosikPatrickPro@gmail.com, or by registered mail with acknowledgment of receipt sent to the publisher at the coordinates specified in these legal notices.
-`;
-
-export default function page({
+export default async function page({
     params: { lang },
 }: {
     params: { lang: Locale };
 }) {
-    const formattedText = formatLegalText(rawLegalText);
+    const dict = await getDictionary(lang);
+    const formattedText = formatLegalText(dict.TemplateLegalNotices.legalText.join('\n'));
     return (
         <MaxWidthWrapper>
             <div className="py-10 px-6 sm:px-10 lg:px-16">
                 {formattedText}
             </div>
         </MaxWidthWrapper>
-    )
+    );
 }
-
-
