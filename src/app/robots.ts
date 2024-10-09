ac67f8ba1dbebf9f58a9f1dec84e80cfc6aff64e
Patrick Bartosik <patrickbartosik@MBP-de-Patrick.lan>
2024-10-09 19:35:54 +0200

    📝 (robots.ts): add trailing slash to sitemap URL for consistency and to prevent potential issues with web crawlers

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
             userAgent: '*',
             allow: '/',
         },
-        sitemap: 'https://patrick.bartosik.fr/sitemap.xml',
+        sitemap: 'https://patrick.bartosik.fr/sitemap.xml/',
     }
 }
