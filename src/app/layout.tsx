0d5d8cd744a6a142765e340baabfc5205de70c91
Patrick Bartosik <patrickbartosik@MBP-de-Patrick.lan>
2024-10-09 18:19:27 +0200

    ✨ (layout.tsx): add noscript tag with Google Tag Manager iframe to provide tracking support for users with disabled JavaScript

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -71,6 +71,14 @@ export default function RootLayout({
         <SpeedInsights />
         <Analytics />
       </body>
+      <noscript>
+        <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=VotreIdVaIr" 
+            height="0" 
+            width="0" 
+            style={{display: 'none', visibility: 'hidden'}}>
+        </iframe>
+      </noscript>
     </html>
   );
 }
